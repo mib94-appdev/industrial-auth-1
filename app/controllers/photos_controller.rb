15d04d3bbae4f6f3423d21ab74d95db1e481544b
mib94 <mibnobachir@chicagobooth.edu>
2023-02-08 01:53:10 +0000

    Comments and likes fixed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
 
-  before_action :ensure_current_user_is_owner, only: [:update, :destroy]
+  before_action :ensure_current_user_is_owner, only: [:edit, :update, :destroy]
 
   def ensure_current_user_is_owner
     if current_user != @photo.owner
